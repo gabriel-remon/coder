289e14777daf51cd0c08eabd225d8cadcc9aa589
gabriel-remon <remongabi16@gmail.com>
2023-03-15 15:49:08 -0300

    re entrega del desafio 2

--- desafio3/index.js
+++ desafio3/index.js
@@ -1,21 +1,29 @@
 import ProductManager from "./producManager.js";
-import express  from "express";
+import express from "express";
 
-const app =express()
-const PORT= 8080;
+const app = express()
+const PORT = 8080;
 const producManager = new ProductManager("./productsTest.json")
 
-app.get("/products", async (req,res)=>{
-const products = await producManager.getProducts();
-if(req.query.limit)
-{
-    return res.send(products.splice(0,req.query.limit))
-}
-res.send(products)
+app.get("/products", async (req, res) => {
+    try {
+        const products = await producManager.getProducts();
+        if (req.query.limit) {
+            return res.send(products.splice(0, req.query.limit))
+        }
+        res.send(products)
+
+    } catch (err) {
+        return res.send(err)
+    }
 })
 
-app.get("/products/:Pid", async (req,res)=>{
-res.send(await producManager.getProductById(req.params.Pid))
+app.get("/products/:Pid", async (req, res) => {
+    try {
+        res.send(await producManager.getProductById(req.params.Pid))
+    } catch (err) {
+        return res.send(err)
+    }
 })
 
-app.listen(PORT,(_)=>console.log(`Server iniciado en puerto= ${PORT}`))
+app.listen(PORT, (_) => console.log(`Server iniciado en puerto= ${PORT}`))
